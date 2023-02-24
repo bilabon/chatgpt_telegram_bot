f467f296a9dfe1b288c83b19545318db9a82dda7
Dmytro Bazas <babyx64@gmail.com>
2023-02-25 01:48:50 +0200

    move some sql to fixtures

---
+++ fixtures/initial.py
@@ -0,0 +1,23 @@
+CREATE_TABLES_SQL = """CREATE TABLE "user_role" ("id" integer NOT NULL PRIMARY KEY AUTOINCREMENT, "name" varchar(10) NOT NULL UNIQUE);
+CREATE TABLE "user" (
+    "id" integer NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "username" varchar(255) NOT NULL UNIQUE,
+    "first_name" varchar(255) NOT NULL,
+    "telegram_id" integer NOT NULL UNIQUE,
+    "language_code" varchar(2) NOT NULL,
+    "time_added" datetime NOT NULL,
+    "role_id" bigint NOT NULL REFERENCES "user_role" ("id") DEFERRABLE INITIALLY DEFERRED
+);
+CREATE TABLE "user_message" (
+    "id" integer NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "text" varchar(10) NOT NULL UNIQUE, "message_id" integer NOT NULL,
+    "time_added" datetime NOT NULL,
+    "user_id" bigint NOT NULL REFERENCES "user" ("id") DEFERRABLE INITIALLY DEFERRED
+);
+CREATE INDEX "user_role_id_c3a87a3d" ON "user" ("role_id");
+CREATE INDEX "user_message_user_id_8a912feb" ON "user_message" ("user_id");
+INSERT INTO `user_role` (`id`, `name`) VALUES
+    (1, 'admin'),
+    (2, 'client'),
+    (3, 'alient'),
+    (4, 'blocked');"""
--- tools/db.py
+++ tools/db.py
@@ -1,41 +1,19 @@
 import os
 from datetime import datetime, timezone
+from telegram import Update
 
 import aiosqlite
 from aiosqlite.core import Connection
 from models import USER_ROLE_CHOICES, User
 from settings.config import ADMIN_USERNAME, DB_NAME
-from telegram import Update
+from fixtures.initial import CREATE_TABLES_SQL
 
 
 async def check_or_create_db() -> None:
     if not os.path.exists(DB_NAME):
         print(DB_NAME + ' not found!')
         async with aiosqlite.connect(DB_NAME) as db:
-            await db.executescript("""
-CREATE TABLE "user_role" ("id" integer NOT NULL PRIMARY KEY AUTOINCREMENT, "name" varchar(10) NOT NULL UNIQUE);
-CREATE TABLE "user" (
-    "id" integer NOT NULL PRIMARY KEY AUTOINCREMENT,
-    "username" varchar(255) NOT NULL UNIQUE,
-    "first_name" varchar(255) NOT NULL,
-    "telegram_id" integer NOT NULL UNIQUE,
-    "language_code" varchar(2) NOT NULL,
-    "time_added" datetime NOT NULL,
-    "role_id" bigint NOT NULL REFERENCES "user_role" ("id") DEFERRABLE INITIALLY DEFERRED
-);
-CREATE TABLE "user_message" (
-    "id" integer NOT NULL PRIMARY KEY AUTOINCREMENT,
-    "text" varchar(10) NOT NULL UNIQUE, "message_id" integer NOT NULL,
-    "time_added" datetime NOT NULL,
-    "user_id" bigint NOT NULL REFERENCES "user" ("id") DEFERRABLE INITIALLY DEFERRED
-);
-CREATE INDEX "user_role_id_c3a87a3d" ON "user" ("role_id");
-CREATE INDEX "user_message_user_id_8a912feb" ON "user_message" ("user_id");
-INSERT INTO `user_role` (`id`, `name`) VALUES
-    (1, 'admin'),
-    (2, 'client'),
-    (3, 'alient'),
-    (4, 'blocked');""")
+            await db.executescript(CREATE_TABLES_SQL)
             await db.commit()
 
 
