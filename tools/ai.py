2ac0514e9d583e27945708be394983ee23244cef
Dmytro Bazas <babyx64@gmail.com>
2023-02-26 23:55:57 +0200

    set temperature=1

--- tools/ai.py
+++ tools/ai.py
@@ -5,7 +5,7 @@ from settings.config import AI_TOKEN
 def ask_chatgpt(message: str) -> str | None:
     openai.api_key = AI_TOKEN
     response = openai.Completion.create(
-        model="text-davinci-003", prompt=message, temperature=0, max_tokens=500)
+        model="text-davinci-003", prompt=message, temperature=1.0, max_tokens=2000)
     print(f'ask_chat_gpt response: {response}')
     if response and response.choices and response.choices[0] and response.choices[0].text:
         if '_split' not in response.choices[0].text:
