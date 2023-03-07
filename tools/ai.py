4594f5f6087e084db299d601872def5d594d03c7
Dmytro Bazas <babyx64@gmail.com>
2023-03-08 00:42:21 +0200

    support 2 GPT models gpt-3.5-turbo and text-davinci-003

--- tools/ai.py
+++ tools/ai.py
@@ -1,25 +1,35 @@
 import logging
 import openai
-from settings.config import AI_TOKEN
+from settings.config import AI_TOKEN, GPT_MODEL
 
 logger = logging.getLogger(__name__)
 
 
 def ask_chatgpt(message: str) -> str | None:
     openai.api_key = AI_TOKEN
-    # uncomment to use the text-davinci-003 model.
-    # response = openai.Completion.create(
-    #     model="text-davinci-003", prompt=message, temperature=1.0, max_tokens=2000)
-    response = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo",
-        messages=[{"role": "user", "content": message}]
-    )
-    logger.info(f'ask_chat_gpt response: {response}')
-    if all([
-        response,
-        response.choices,
-        response.choices[0],
-        response.choices[0].message,
-        response.choices[0].message.content
-    ]):
-        return response.choices[0].message.content
+    response, text = None, None
+    if GPT_MODEL == "text-davinci-003":
+        response = openai.Completion.create(
+            model="text-davinci-003",
+            prompt=message,
+            temperature=1.0,
+            max_tokens=2000,
+        )
+        if response and response.choices and response.choices[0] and response.choices[0].text:
+            if '_split' not in response.choices[0].text:
+                text = response.choices[0].text
+    elif GPT_MODEL == "gpt-3.5-turbo":
+        response = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=[{"role": "user", "content": message}],
+        )
+        if all([
+            response,
+            response.choices,
+            response.choices[0],
+            response.choices[0].message,
+            response.choices[0].message.content
+        ]):
+            text = response.choices[0].message.content
+    logger.info(f'ask_chat_gpt() {GPT_MODEL} response: {response}')
+    return text
