bc08458dd909ad7fe63763d59096ba48e0955710
Dmytro Bazas <babyx64@gmail.com>
2023-03-08 03:41:17 +0200

    bugfix

--- tools/parser.py
+++ tools/parser.py
@@ -17,4 +17,4 @@ async def parse_setrole_message(message: str) -> tuple:
 
 
 async def parse_context_message(message: str) -> bool:
-    return True if "on" in message.lower() else False
+    return True if " on" in message.lower() else False
