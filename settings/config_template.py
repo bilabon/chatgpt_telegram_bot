b1784a82bafe5b2e2ecc1b62522de6c4befdb8d8
Dmytro Bazas <babyx64@gmail.com>
2023-03-11 19:00:07 +0200

    update comments

--- settings/config_template.py
+++ settings/config_template.py
@@ -2,6 +2,6 @@ DB_NAME = 'db.sqlite'
 BOT_TOKEN = 'setup in config.py'
 AI_TOKEN = 'setup in config.py'
 ADMIN_USERNAME = 'bilabon'
-GPT_MODEL = 'gpt-3.5-turbo'  # supported gpt-3.5-turbo or text-davinci-003
+GPT_MODEL = 'gpt-3.5-turbo'  # support gpt-3.5-turbo or text-davinci-003
 
-# You need to create config.py and set all this variables there
+# You need to create config.py and set all these variables there
