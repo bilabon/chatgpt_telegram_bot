d3aebee045476637bac2eb07ea52144b651c8f15
Dmytro Bazas <babyx64@gmail.com>
2023-03-15 21:40:41 +0200

    minor fix #2

--- main.py
+++ main.py
@@ -159,7 +159,7 @@ async def message_handle(update: Update, context: ContextTypes.DEFAULT_TYPE, use
 
     if text:
         await save_message(user_id=user.id, data=response, text=text)
-        await inform_used_tokens_on_message(update, response)
+        # await inform_used_tokens_on_message(update, response)
         parse_mode = ParseMode.MARKDOWN if user.mode_id == user.get_mode_choices['code_assistant'] else ParseMode.HTML
         await update.message.reply_text(text, parse_mode=parse_mode)
     else:
