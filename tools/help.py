aa605172e143bf3f81de47753652fabd827eb799
Dmytro Bazas <babyx64@gmail.com>
2023-03-15 22:53:46 +0200

    update help message

--- tools/help.py
+++ tools/help.py
@@ -1,9 +1,11 @@
 HELP_MESSAGE = """Commands:
 ⚪ /mode – Select chat mode
-⚪ /contexton - Turn on or clear context
-⚪ /contextoff - Turn off context
+⚪ /contexton - Remember messages enable
+⚪ /contextoff - Remember messages disable
 ⚪ /balance - See you balance
 ⚪ /help – Show help
+
+Keep in mind that the command /contexton will increase the cost of the question.
 """
 
 HELP_MESSAGE_ADMIN = """
