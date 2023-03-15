9fb7b000f5c62660ca00aeaf46fe3e11e0a7c987
Dmytro Bazas <babyx64@gmail.com>
2023-03-15 21:35:15 +0200

    add balances, add FREE_TOKENS logic

--- tools/parser.py
+++ tools/parser.py
@@ -2,7 +2,7 @@ import re
 from models import USER_ROLE_CHOICES
 
 
-async def parse_setrole_message(message: str) -> tuple:
+async def parse_setrole_command(message: str) -> tuple:
     message = re.sub(r"\s+", " ", message).strip()
     if not message.startswith("/setrole "):
         return "Wrong command!", "", ""
@@ -11,6 +11,19 @@ async def parse_setrole_message(message: str) -> tuple:
     if role not in USER_ROLE_CHOICES:
         return "Wrong role name!", "", ""
     return "", username, role
+
+
+async def parse_addbalance_command(message: str) -> tuple:
+    message = re.sub(r"\s+", " ", message).strip()
+    if not message.startswith("/addbalance "):
+        return "Wrong command!", "", ""
+
+    _, username, tokens = message.split(maxsplit=2)
+    try:
+        tokens = int(tokens)
+    except Exception as err:
+        return f"Wrong tokens! {err}", "", ""
+    return "", username, tokens
 
 
 async def parse_context_message(message: str) -> bool:
---
+++ tools/utils.py
@@ -0,0 +1,24 @@
+from telegram import Update
+from telegram.constants import ParseMode
+from openai.openai_object import OpenAIObject
+
+from models import User
+from settings.config import ADMIN_USERNAME
+
+
+async def inform_used_tokens_on_message(update: Update, response: OpenAIObject) -> None:
+    text = f"🟢 You spent on last message <b>{response.usage.total_tokens}</b> tokens."
+    await update.message.reply_text(text, parse_mode=ParseMode.HTML)
+
+
+async def show_user_balance(update: Update, user: User, only_reply_if_negative_balance=False) -> str | None:
+    balance_spent = await user.get_balance_spent()
+    balance_credited = await user.get_balance_credited()
+    balance_left = balance_credited - balance_spent
+    text = "🟢 " if balance_left > 0 else "🔴 "
+    text += f"You have <b>{balance_left}</b> tokens left. You totally spent <b>{balance_spent}</b> tokens." \
+            f"\n\nWrite to admin @{ADMIN_USERNAME} to top up your balance."
+    if only_reply_if_negative_balance and balance_left > 0:
+        # if balance_left > 0 and only_reply_if_negative_balance == True we do not return reply_text
+        text = None
+    return text
